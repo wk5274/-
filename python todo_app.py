f3b315101cb816ad2f2ad47e82abce14bfd7a262
wk5274 <95701463+wk5274@users.noreply.github.com>
2024-03-12 23:47:26 +0800

    Create python todo_app.py

---
+++ python todo_app.py
@@ -0,0 +1,28 @@
+$ python todo_app.py
+
+Add Task
+Remove Task
+Display Tasks
+Exit
+Enter your choice: 1
+Enter task: Buy groceries
+
+Add Task
+Remove Task
+Display Tasks
+Exit
+Enter your choice: 3
+Todo List:
+
+Buy groceries
+
+Add Task
+
+Remove Task
+
+Display Tasks
+
+Exit
+
+Enter your choice: 4
+Exiting...
